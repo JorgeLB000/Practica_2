b73631054c37f29d510c950df791d603ad1e79b3
JorgeLB000 <jlariob@iesch.org>
2020-10-14 17:30:23 +0200

    v1

--- app/src/main/java/org/iesch/practica2/MainActivity.java
+++ app/src/main/java/org/iesch/practica2/MainActivity.java
@@ -3,6 +3,10 @@ package org.iesch.practica2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView punto = (TextView) findViewById(R.id.punto);
+        final TextView pregunta = (TextView) findViewById(R.id.pregunta);
+        final RadioButton rb1 = (RadioButton) findViewById(R.id.rb1);
+        final RadioButton rb2 = (RadioButton) findViewById(R.id.rb2);
+        final ImageView imgView1 = (ImageView) findViewById(R.id.imgView1);
+        final ImageView imgView2 = (ImageView) findViewById(R.id.imgView2);
+        final Button btn = (Button) findViewById(R.id.btn);
+        final int point = 1;
     }
 }
