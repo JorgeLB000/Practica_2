46b013f81be24b740dfa76a63b523904efab4680
JorgeLB000 <jlariob@iesch.org>
2020-10-16 18:23:24 +0200

    v5

--- app/src/main/java/org/iesch/practica2/CargaActivity.java
+++ app/src/main/java/org/iesch/practica2/CargaActivity.java
@@ -1,5 +1,14 @@
 package org.iesch.practica2;
 
-public class CargaActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+public class CargaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga);
+    }
 }
